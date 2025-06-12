c5fc074e922a875a2f2d49a1044805b51e245c6d
mahsabeigi33 <mahsabeigi33@gmail.com>
2025-06-11 20:54:14 -0600

    Handle baseURL for backend Server

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
@@ -10,7 +10,6 @@ export const api = axios.create({
     validateStatus: (status) => {
         return status >= 200 && status < 300; 
     }
-
 });
 api.interceptors.request.use(
   (config) => {
